300eb647e6fbf540f210e782f0ceca0604bf2231
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2024-03-01 08:03:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string.h>
+#include <iomanip>
+#include <ctype.h>
 using namespace std;
 
 enum MenuItem {
@@ -39,28 +41,21 @@ struct Orders {
 	Reservations reservation;
 	Tables table;
 	MenuItem items[10];
-	int numItems;
+	string entreName[10];
+	bool isReady = false;
 	bool complete = false;
 	bool paid = false;
 } order;
 
+
+//---- Global Variables ----//
 Reservations reservations[50];
 Tables tables[20];
 Orders orders[50];
-
-struct Bills
-{
-	Tables tables;
-	Orders order;
-	int subtotal;
-	int taxes = 0.07;
-	int tip = 0.20;
-	int total;
-} bill;
-
 int numReservations = 0;
 int numTables = 20;
-int numOrders = 50;
+int numOrders = 0;
+//-------------------------//
 
 void welcomeMessage()
 {
@@ -75,14 +70,31 @@ void welcomeMessage()
 
 void createReservation() {
 
-	if (numReservations >= 50)
+	if (numReservations > 50)
 	{
 		cout << "You are at the maximum reservations!" << endl;
 	}
 	else
 	{
 		cout << "Enter a name for the reservation: "; cin >> reservation.firstName >> reservation.lastName;
-		cout << "\nEnter the number of people in the party: "; cin >> reservation.reservationSize;
+		do { 
+			cout << "\nEnter the number of people in the party: "; cin >> reservation.reservationSize;
+			if (cin.fail())
+			{
+				if (reservation.reservationSize > 10)
+				{
+					cout << "The maximum party size is 10. Try again :(" << endl;
+				}
+				if (isdigit(reservation.reservationSize) == 0)
+				{
+					cout << "\nYou must enter an integer.\n";
+				}
+				cin.clear();
+				cin.ignore(1200, '\n');
+
+			}
+				
+		} while (reservation.reservationSize > 10 || isdigit(reservation.reservationSize) == 0);
 		cout << "\nEnter the time for the reservation in HH:MM AM/PM: "; cin >> reservation.time >> reservation.amORpm;
 		reservation.checkedIn = false;
 	}
@@ -99,6 +111,12 @@ void confirmReservation()
 		cout << "\nNumber in Party: " << reservation.reservationSize;
 		cout << "\nIs this information correct [Y]es, [N]o (make changes), [C]ancel? ";
 		cin >> choice;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1200, '\n');
+			cout << "You must enter a letter (Y/N/C) and the entry must be captialized.\n";
+		}
 		if (choice == 'Y')
 		{
 			reservations[numReservations] = reservation;
@@ -111,6 +129,12 @@ void confirmReservation()
 				<< "2. Time\n"
 				<< "3. Number of people";
 			cin >> response;
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(1200, '\n');
+				cout << "Enter an integer to the corresponding option.\n";
+			}
 			switch (response)
 			{
 			case 1:
@@ -158,46 +182,62 @@ void checkInReservation()
 			}
 		}
 		cin >> chosenReservation;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1200, '\n');
+			cout << "Enter an integer to the corresponding reservation.\n";
+		}
 		reservations[chosenReservation - 1].checkedIn = true;
 		cout << "Reservation checked in successfully!\n";
 		cout << "Choose a table to be seated:\n";
 		for (int j = 0; j <= numTables; j++)
 		{
-			if (tables[j].available && tables[j].size >= reservations[chosenReservation - 1].reservationSize)
+			if (orders[j].table.available && orders[j].table.size >= reservations[chosenReservation - 1].reservationSize)
 			{
-				cout << tables[j].tableNumber << ". " << tables[j].size << " people\n";
+				cout << orders[j].table.tableNumber << ". " << orders[j].table.size << " people\n";	
+				cin >> selectedTable;
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(1200, '\n');
+					cout << "Enter an integer to the corresponding table number.\n";
+				}
 			}
+			else { cout << "\nThere are no tables available to be sat.\n"; }
 		}
-		cin >> selectedTable;
-		tables[selectedTable - 1].available = false;
+		
+		orders[selectedTable - 1].table.available = false;
 
 		// this sets the size of the table equal to the size of the reservation
-		order.table.tableNumber = selectedTable;
-		tables[selectedTable - 1].numberOfPeople = reservations[chosenReservation - 1].reservationSize;
+		orders[selectedTable - 1].table.numberOfPeople = reservations[chosenReservation - 1].reservationSize;
 	}
 	else { cout << "You must make a reservation first." << endl; }
 }
 
 void createOrder()
 {
+	int j = 0;
 	int entreSelection;
 	int tableToOrder;
 	cout << "Please select the table that is ready to order: \n";
 	for (int i = 0; i < numTables; i++)
 	{
-		if (tables[i].numberOfPeople > 0)
+		if (orders[i].isReady == false && orders[i].table.numberOfPeople > 0)	
 		{
-			cout << tables[i].tableNumber << ". Table for " << tables[i].numberOfPeople << " people.\n";
+			cout << orders[i].table.tableNumber << ". Table for " << orders[i].table.numberOfPeople << " people.\n";
 		}
 	}
 	cin >> tableToOrder;
-
-	// adding table number and number of people inside the table array to the order object inside the Orders struct
-	order.table.tableNumber = tables[tableToOrder - 1].tableNumber;
-	order.table.numberOfPeople = tables[tableToOrder - 1].numberOfPeople;
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(1200, '\n');
+		cout << "Enter an integer to the corresponding table number.\n";
+	}
 	if (numOrders < 51)
 	{
-		for (int j = 0; j < order.table.numberOfPeople; j++)
+		while (j < orders[tableToOrder-1].table.numberOfPeople)
 		{
 			cout << "Please choose for entre number " << j + 1 << ":\n";
 			cout << "1: Veggie Burger\n"
@@ -215,100 +255,167 @@ void createOrder()
 				<< "13: Faux Meat adn chips\n"
 				<< "14: Fish and Chips\n";
 			cin >> entreSelection;
-
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(1200, '\n');
+				cout << "Enter an integer between 1 and 14\n";
+			}
 			switch (entreSelection)
 			{
 			case 1:
 				entreSelection = VEGGIE_BURGER;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Veggie Burger";
 				numOrders++;
 				break;
 			case 2:
 				entreSelection = HAMBURGER;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Hamburger";
 				numOrders++;
 				break;
 			case 3:
 				entreSelection = FRIED_CHICKEN_SLIDERS;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Fried Chicken Sliders";
 				numOrders++;
 				break;
 			case 4:
 				entreSelection = HAMBURGER_SLIDERS;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Hamburger Sliders";
 				numOrders++;
 				break;
 			case 5:
 				entreSelection = VEGGIE_SLIDERS;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Veggie Sliders";
 				numOrders++;
 				break;
 			case 6:
 				entreSelection = HADDOCK_SANDWICH;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Haddock Sandwhich";
 				numOrders++;
 				break;
 			case 7:
 				entreSelection = BLT;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "BLT";
 				numOrders++;
 				break;
 			case 8:
 				entreSelection = FAUX_BLT;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Faux BLT";
 				numOrders++;
 				break;
 			case 9:
 				entreSelection = CHICKEN_BURRITO;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Chicken Burrito";
 				numOrders++;
 				break;
 			case 10:
 				entreSelection = FALAFEL_WRAP;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Falafel Wrap";
 				numOrders++;
 				break;
 			case 11:
 				entreSelection = CHEESE_PIZZA;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Cheese Pizza";
 				numOrders++;
 				break;
 			case 12:
 				entreSelection = PEPPERONI_PIZZA;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Pepperoni Pizza";
 				numOrders++;
 				break;
 			case 13:
 				entreSelection = FAUX_MEAT_AND_CHIPS;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Faux Meat and Chips";
 				numOrders++;
 				break;
 			case 14:
 				entreSelection = FISH_AND_CHIPS;
-				order.items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].items[j] = static_cast<MenuItem>(entreSelection);
+				orders[tableToOrder - 1].entreName[j] = "Fish and Chips";
 				numOrders++;
 				break;
 			}
-
-		}
-		orders[tableToOrder] = order;
+			j++;
+		}	
+	}else {
+		cout << "The restaurant is completely full!! Have people spend pay their bill and leave." << endl;
 	}
-		
-	
-}
+	orders[tableToOrder - 1].isReady = true;
+	cout << "\n" << numOrders;	
+}	
 
 void completeOrder()
 {
 	int chosenTable;
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < numTables; i++)
 	{
-		if (orders[i].table.numberOfPeople > 0 )
+		if (orders[i].table.numberOfPeople > 0 && orders[i].complete == false)	
 		{
-			cout << "Table Number: " << orders[i].table.tableNumber << " Table for " << orders[i].table.numberOfPeople << "\n";
+			cout << orders[i].table.tableNumber << ". Table for " << orders[i].table.numberOfPeople << endl;
+			
 		}
 	}
 	cin >> chosenTable;
+	cout << "Here is your order: " << endl;
+	for (int i = 0; i < orders[chosenTable - 1].table.numberOfPeople; i++)
+	{
+		cout << "\t" << orders[chosenTable - 1].entreName[i] << ": $" << orders[chosenTable - 1].items[i] << endl;
+
+	}
 	orders[chosenTable - 1].complete = true;
+}
+
+void calculateBill()
+{
+	double subtotal;
+	double taxes = 0.10;
+	double tip = 0.20;
+	int selectedTable;
+	for (int i = 0; i < 50; i++)
+	{
+		if (orders[i].complete == true && orders[i].paid == false)
+		{
+			cout << orders[i].table.tableNumber <<
+				". Table for " <<
+				orders[i].table.numberOfPeople << endl;
+		}
+	}
+	cin >> selectedTable;
+
+	// calculate subtotal
+	for (int i = 0; i < orders[selectedTable - 1].table.numberOfPeople; i++)
+	{
+		subtotal += orders[selectedTable - 1].items[i];
+	}
+	cout << "Here is your order: " << endl;
+	for (int i = 0; i < orders[selectedTable - 1].table.numberOfPeople; i++)
+	{
+		cout << "\t" << orders[selectedTable - 1].entreName[i] << ": $" << orders[selectedTable - 1].items[i] << endl;
+
+	}
+	cout << "\tSubtotal: $" << subtotal << endl;
+	cout << "\tTip: $" << subtotal * tip << endl;
+	cout << "\tTaxes: $" << subtotal * taxes << endl;
+	cout << "\tTotal: $" << fixed << setprecision(2) << ((subtotal*tip) + (subtotal*taxes) + (subtotal)) << endl;
+	orders[selectedTable - 1].paid = true;
+	orders[selectedTable - 1].table.available = true;
+	orders[selectedTable - 1].complete = false;
+	orders[selectedTable - 1].isReady = false;
+	numOrders -= orders[selectedTable - 1].table.numberOfPeople;
 }
 
 int main() 
@@ -320,7 +427,7 @@ int main()
 	for (int i = 0; i < numTables; i++)
 	{
 
-		//tbales[i] = {i + 1, 2, 0, true}
+		//tables[i] = {i + 1, 2, 0, true}
 
 		if (i < 8)
 		{
@@ -328,7 +435,7 @@ int main()
 			table.size = 2;
 			table.numberOfPeople = 0;
 			table.available = true;
-			tables[i] = table;
+			orders[i].table = table;
 		}
 		else if (i < 14)
 		{
@@ -336,7 +443,7 @@ int main()
 			table.size = 4;
 			table.numberOfPeople = 0;
 			table.available = true;
-			tables[i] = table;
+			orders[i].table = table;
 		}
 		else if (i < 18)
 		{
@@ -344,20 +451,25 @@ int main()
 			table.size = 6;
 			table.numberOfPeople = 0;
 			table.available = true;
-			tables[i] = table;
+			orders[i].table = table;
 		}
 		else {
 			table.tableNumber = i + 1;
 			table.size = 10;
 			table.numberOfPeople = 0;
 			table.available = true;
-			tables[i] = table;
+			orders[i].table = table;
 		}
 	}
 	do {
 		welcomeMessage();
 		cin >> response;
-
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1200, '\n');
+			cout << "Enter an integer between 1 and 6\n";
+		}
 		switch (response)
 		{
 		case 1:
@@ -374,11 +486,11 @@ int main()
 			completeOrder();
 			break;
 		case 5:
-			//calculateBil();
-			//payBill();
+			calculateBill();
 			break;
 		case 6:
 			cout << "Store is now closed. Good Bye!";
 		}
 	} while (response != 6);
+	
 }
